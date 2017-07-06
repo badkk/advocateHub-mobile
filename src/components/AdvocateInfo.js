d8237af38df78ff1fece4b414d89ec3fa2c8d469
ZikunFan <zikunvan@gmail.com>
2017-07-06 18:05:08 +0800

    change advocate info(change follow style)

--- src/components/AdvocateInfo.js
+++ src/components/AdvocateInfo.js
@@ -1,20 +1,25 @@
 import React, {Component} from 'react'
 import Menu from './commons/Menu'
-import {Paper, IconButton, FontIcon, MenuItem, IconMenu, Avatar, ListItem} from 'material-ui'
+import {Paper, IconButton, FontIcon, MenuItem, IconMenu, Avatar, ListItem, AppBar, FloatingActionButton, RaisedButton, List, Divider} from 'material-ui'
 import {cyan500} from 'material-ui/styles/colors';
+import "../styles/AdvocateInfo.css"
+import { BottomSheet } from 'material-ui-bottom-sheet';
+import { SocialPersonAdd } from 'material-ui/svg-icons'
+import {grey500} from 'material-ui/styles/colors'
+
 /**
  * Created by lucas on 2017/7/4.
  * Advocate Info Page
  */
-const addPerson = <FontIcon className="material-icons">person_add</FontIcon>;
-
 class AdvocateInfoBar extends Component{
     constructor(props) {
         super(props);
         this.state={
-            open: false
+            open: false,
+            isOpen: false
         };
         this.handleMenuClick = this.handleMenuClick.bind(this);
+        this.handleBSPopout = this.handleBSPopout.bind(this);
     }
     handleMenuClick(e, item) {
         const itemName = item.key === '0' ? "Twitter" : item.key === '1' ? "Github" : "Facebook";
@@ -23,32 +28,54 @@ class AdvocateInfoBar extends Component{
             open: true
         })
     }
+    handleBSPopout() {
+        this.setState({
+            isOpen: true
+        })
+    }
+
     render() {
         const iconButtonElement = (
-            <IconButton
-                touch={true}
-                tooltip="more"
-                tooltipPosition="bottom-left">
-                <FontIcon className="material-icons" color={cyan500}>add_circle</FontIcon>;
-            </IconButton>
+            <RaisedButton primary={true} className="follow-button" label="Follow" onTouchTap={this.handleBSPopout}/>
         );
         const rightIconMenu = (
             <IconMenu iconButtonElement={iconButtonElement}  onItemTouchTap={this.handleMenuClick}>
-                <MenuItem rightIcon={addPerson} key={0}>Twitter</MenuItem>
-                <MenuItem rightIcon={addPerson} key={1}>Github</MenuItem>
-                <MenuItem rightIcon={addPerson} key={2}>Facebook</MenuItem>
+                <MenuItem rightIcon={<SocialPersonAdd/>} key={0}>Twitter</MenuItem>
+                <MenuItem rightIcon={<SocialPersonAdd/>} key={1}>Github</MenuItem>
+                <MenuItem rightIcon={<SocialPersonAdd/>} key={2}>Facebook</MenuItem>
             </IconMenu>
         );
         const height = window.screen.height * 0.12;
+        /* Bottom shared sheet */
+        const sharedBottomSheet = <div>
+            <BottomSheet
+                action={
+                    <FloatingActionButton>
+                        <SocialPersonAdd/>
+                    </FloatingActionButton>
+                }
+                onRequestClose={() => this.setState({isOpen: false})}
+                open={this.state.isOpen}>
+                <h4 style={{color: grey500, marginLeft: '25px'}}>Share this meeting</h4>
+                <Divider inset/>
+                <List>
+                    <ListItem primaryText="Facebook" leftIcon={<SocialPersonAdd/>}/>
+                    <ListItem primaryText="Twitter" leftIcon={<SocialPersonAdd/>}/>
+                    <ListItem primaryText="Google+" leftIcon={<SocialPersonAdd/>}/>
+                </List>
+            </BottomSheet>
+        </div>;
         return (
             <Paper style={{maxHeight: height}}>
+
                 <ListItem
                     primaryText="John Papa"
                     secondaryText="Node.js, .NET, React"
                     leftAvatar={<Avatar src="johnpapa.png" />}
-                    rightIconButton={rightIconMenu}
+                    rightIconButton={iconButtonElement}
                     style={{width:"100%"}}
                 />
+                {sharedBottomSheet}
             </Paper>
         );
     }
@@ -60,12 +87,10 @@ export default class AdvocateInfo extends Component {
             history: this.props.history
         }
     }
+    handleBack = () => {
+        this.state.history.goBack();
+    };
     render() {
-        const personalGenPanel = (
-            <Paper>
-
-            </Paper>
-        );
         const iFramePanelHeight = window.screen.height * 0.8;
         return (
             <div>
