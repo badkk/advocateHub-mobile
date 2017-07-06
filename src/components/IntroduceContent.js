25b0f0be5445236d30ece534ec783a037fd6beb8
ZikunFan <zikunvan@gmail.com>
2017-07-06 18:05:32 +0800

    Complete Introduce Content

--- src/components/IntroduceContent.js
+++ src/components/IntroduceContent.js
@@ -2,18 +2,22 @@ import React, {Component} from 'react'
 import Slider from 'react-slick'
 import _ from 'underscore'
 import '../styles/IntroduceContent.css'
+import {Card, CardHeader, CardMedia, CardText, CardTitle, CardActions, FlatButton} from 'material-ui'
 /**
  * Created by t-zikfan on 2017/7/5.
  * Meeting page Introduction Content Page
  */
 export default class IntroduceContent extends Component {
+    handleLearnMoreClick = () => {
+      window.location = "https://johnpapa.net/azure-and-angular-on-dotnetrocks/";
+    };
     render() {
         /* Carousel */
         const settings = {
             dots: true,
+            arrows: false,
             infinite: true,
-            speed: 1000,
-            autoplaySpeed: 5000,
+            speed: 500,
             slidesToShow: 1,
             slidesToScroll: 1,
             autoplay: true
@@ -24,7 +28,9 @@ export default class IntroduceContent extends Component {
                 <div>
                     <img src={img} style={{height: {imgHeight}}}/>
                     <div className='introduce-carousel-text-panel'>
-                        <h4 style={{margin:0}}>Deploying Angular to Azure {idx}</h4>
+                        <span style={{margin:0, fontSize: '18px', fontWeight: 'bold'}}>Deploying Angular to Azure {idx}</span>
+                        <br/>
+                        <span>Content about Slide {idx}</span>
                     </div>
                 </div>
             );
@@ -39,8 +45,27 @@ export default class IntroduceContent extends Component {
         );
         /*  */
         return (
-            <div className="">
-                {carouselContent}
+            <div style={{maxHeight: this.props.maxHeight}}>
+                <Card>
+                    <CardHeader
+                        title="Deploying Angular to Azure"
+                        subtitle="10:30 Wed 7/5/2017 by John Papa"
+                        titleStyle={{fontSize:'20px', fontWeight:'bold', marginBottom:'10px'}}
+                        className="card-header"
+                    />
+                    <CardMedia>
+                        {carouselContent}
+                    </CardMedia>
+                    <CardTitle title="Quick learn" subtitle=""/>
+                    <hr style={{marginLeft:'16px'}}/>
+                    <CardText>
+                        The Angular CLI makes it easy to build a production ready Angular app. The next step is getting that app up and in the cloud. This is where a CI process helps take that code from Github, build it properly, and the deploy it to Azure. I outlined the detailed steps below if you want to try this for yourself.
+                    </CardText>
+                    <CardActions>
+                        <FlatButton primary={true} label="Learn more" onTouchTap={this.handleLearnMoreClick}/>
+                    </CardActions>
+                </Card>
+
             </div>
         );
     }
