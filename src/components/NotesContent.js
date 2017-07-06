80cf25bc90f41c9fcb56d9db3a1447430735d979
ZikunFan <zikunvan@gmail.com>
2017-07-06 18:07:55 +0800

    add Note content page

---
+++ src/components/NotesContent.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import 'github-markdown-css'
+import MarkdownContent from '../res/md'
+
+const ReactMarkdown = require('react-markdown');
+/**
+ * Created by t-zikfan on 2017/7/6.
+ */
+
+export default class NotesContent extends Component {
+    render() {
+        const content = MarkdownContent.art1;
+        return (
+            <div className="content-panel" style={{maxHeight: this.props.maxHeight}}>
+                <ReactMarkdown className="markdown-body" source={content} softBreak="br" sourcePos={true}/>
+            </div>);
+    }
+}
