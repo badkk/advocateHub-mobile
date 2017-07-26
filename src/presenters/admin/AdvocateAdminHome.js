f7efedd59c6c415c463dfffab47623f109c7abb7
jehuan <jehuan@microsoft.com>
2017-07-26 14:55:46 +0800

    Add more meeting info

--- src/presenters/admin/AdvocateAdminHome.js
+++ src/presenters/admin/AdvocateAdminHome.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react'
 import AdminAppBar from "../commons/AdminAppBar";
 import { RaisedButton } from 'material-ui'
 import AdvocateMeetingInfo from './AdvocateMeetingInfo.js'
+import post from '../../restful/Post'
+import * as _ from "underscore";
+
 /**
  * Created by t-zikunfan
  * Date: 11:03 2017/7/25
@@ -11,19 +14,24 @@ export default class AdvocateAdminHome extends Component {
         super(props);
         this.state = {
             showForm: false,
-            userId: this.props.match.params.userId,
+            meethingInfo: { "advocatorId" : this.props.match.params.userId },
         };
-        this.CreateMeeting = this.CreateMeeting.bind(this);
-        this.CancelMeeting = this.CancelMeeting.bind(this);
+        this.createMeeting = this.createMeeting.bind(this);
+        this.cancelMeeting = this.cancelMeeting.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.postMeeting = this.postMeeting.bind(this);
     }
+
     render() {
         var component;
-        const buttonLabel = this.state.showForm ? "Cancel meeting" : "Create meeting";
-        const buttonEvent = this.state.showForm ? this.CancelMeeting : this.CreateMeeting;
+        const buttonLabel = this.state.showForm ? "Cancel" : "Create meeting";
+        const buttonEvent = this.state.showForm ? this.cancelMeeting : this.createMeeting;
+
         if(this.state.showForm){
             component = <div>
+                <AdvocateMeetingInfo handleChange={this.handleChange}/>
+                <RaisedButton label="Create" onTouchTap={this.postMeeting} primary={true} fullWidth={true} />
                 <RaisedButton label={buttonLabel} onTouchTap={buttonEvent} primary={true} fullWidth={true} />
-                <AdvocateMeetingInfo userId={this.state.userId}/>
             </div>;
         } else {
             component = <RaisedButton label={buttonLabel} onTouchTap={buttonEvent} primary={true} fullWidth={true}/>;
@@ -36,11 +44,26 @@ export default class AdvocateAdminHome extends Component {
         );
     }
 
-    CreateMeeting() {
+    createMeeting() {
         this.setState({showForm: true});
     }
 
-    CancelMeeting() {
+    cancelMeeting() {
         this.setState({showForm: false});
+    }
+
+    handleChange(key, value) {
+        let old = this.state.meethingInfo;
+        old[key] = value;
+        this.setState({meethingInfo: old})
+    }
+
+    postMeeting(){
+        post('/meeting/create', this.state.meethingInfo).then(res => {
+            if (!_.isEmpty(res) && res['data'] === true) {
+                // TODO: redirection?
+                console.log("success!");
+            }
+        });
     }
 }
--- src/presenters/admin/AdvocateMeetingInfo.js
+++ src/presenters/admin/AdvocateMeetingInfo.js
@@ -1,37 +1,58 @@
-import React from 'react'
-import {DatePicker, TimePicker, TextField} from 'material-ui'
+import React, {Component} from 'react'
+import {DatePicker, TimePicker, TextField, SelectField, MenuItem} from 'material-ui'
 
-export default function AdvocateMeetingInfo({userId}) {
-    const form = (
-        <div className="meeting-info-form">
-            <TextField
-                id="meeting_title"
-                floatingLabelText="Title"
-            />
-            <DatePicker
-                hintText="Date"
-                autoOk="true"
-            />
-            <TimePicker
-                hintText="Time"
-                format="24hr"
-                autoOk="true"
-            />
-            <TextField
-                id="meeting_location"
-                floatingLabelText="Location"
-            />
-            <TextField
-                id="meeting_description"
-                floatingLabelText="Description"
-            />
-        </div>);
+export default class AdvocateMeetingInfo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            typeValue: 1
+        }
+    }
 
-    return (
-        <div className="meeting-info">
-            <div style={{marginLeft: '10%', marginRight: '10%', width: '100%'}}>
-                {form}
+    render() {
+        const {handleChange}= this.props;
+        const form = (
+            <div className="meeting-info-form">
+                <TextField
+                    id="meeting_title"
+                    floatingLabelText="Title"
+                    onChange={event => handleChange("name", event.target.value)}
+                />            
+                <DatePicker
+                    hintText="Date"
+                />
+                <TimePicker
+                    hintText="Time"
+                    format="24hr"
+                />
+                <TextField
+                    id="meeting_location"
+                    floatingLabelText="Location"
+                    onChange={event => handleChange("location", event.target.value)}
+                />
+                <TextField
+                    id="meeting_description"
+                    floatingLabelText="Description"
+                    onChange={event => handleChange("description", event.target.value)}
+                />
+                 <SelectField
+                    floatingLabelText="Type"
+                    value={this.state.typeValue}
+                    onChange={(event, index, value) => {
+                        this.setState({typeValue: value});
+                        handleChange("type", value)}
+                    }>
+                    <MenuItem value={1} primaryText="online" />
+                    <MenuItem value={2} primaryText="offline" />
+                </SelectField>  
+            </div>);
+
+        return (
+            <div className="meeting-info">
+                <div style={{marginLeft: '10%', marginRight: '10%', width: '100%'}}>
+                    {form}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
