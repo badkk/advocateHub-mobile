c715f859ad1ef644ecd7cf8a4a29a64a17b066fb
ZikunFan <zikunvan@gmail.com>
2017-08-02 19:03:21 +0800

    change introduction page style

--- src/presenters/meetingdetail/IntroduceContent.js
+++ src/presenters/meetingdetail/IntroduceContent.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Card, CardHeader, CardTitle} from 'material-ui'
+import {CommunicationLocationOn, ActionAlarm} from 'material-ui/svg-icons'
 /**
  * Created by t-zikfan on 2017/7/5.
  * Meeting page Introduction Content Page
@@ -9,10 +10,19 @@ export default function IntroduceContent({meeting}) {
         <Card style={{height: 'auto'}} zDepth={0} className="introduce-panel">
             <CardHeader
                 title={meeting.name}
-                subtitle={"Created on " + new Date(meeting.date).toString().substring(0, 10) + " by " + meeting.advocator.name}
                 titleStyle={{fontSize:'20px', marginBottom:'10px'}}
                 className="card-header"
             />
+            <div className="meeting-introduction-icon-panel"><ActionAlarm/> {new Date(meeting.date).toLocaleString()}</div>
+            <div className="meeting-introduction-icon-panel"><CommunicationLocationOn/> {meeting.location}</div>
+            <div
+                style={{
+                    margin: '20px 0 10px 16px',
+                    fontSize: '17px',
+                    color: 'dimgrey'
+                }}
+            > Summary </div>
+            <hr />
             <CardTitle
                 subtitle={meeting.description}/>
         </Card>
