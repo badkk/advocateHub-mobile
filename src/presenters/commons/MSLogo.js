f0d23e71dfe541adaad1630302e50195e5acb05a
ZikunFan <zikunvan@gmail.com>
2017-08-24 17:39:04 +0800

    change app logo className to inline css

--- src/presenters/commons/MSLogo.js
+++ src/presenters/commons/MSLogo.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {SvgIcon, IconButton} from 'material-ui'
+import {appBarClasses} from "../../styles/AppBarStyles";
+
 /**
  * Created by t-zikunfan
  * Date: 11:02 2017/7/21
@@ -10,13 +12,13 @@ export default function MSLogo({size, logoEvent}) {
         <IconButton onTouchTap={logoEvent} style={{padding: 0}}>
             <SvgIcon
                 viewBox="0 0 64 64"
-                className="icon"
-                style={{height: size + 'px', width: size + 'px', marginLeft: 0}}
+                className={appBarClasses.msLogo}
+                style={{height: size + 'px', width: size + 'px'}}
             >
-                <path className="st0" d="M0 0h30v30H0z"/>
-                <path className="st1" d="M34 0h30v30H34z"/>
-                <path className="st2" d="M34 34h30v30H34z"/>
-                <path className="st3" d="M0 34h30v30H0z"/>
+                <path className={appBarClasses.msLogoSvgSt0} d="M0 0h30v30H0z"/>
+                <path className={appBarClasses.msLogoSvgSt1} d="M34 0h30v30H34z"/>
+                <path className={appBarClasses.msLogoSvgSt2} d="M34 34h30v30H34z"/>
+                <path className={appBarClasses.msLogoSvgSt3} d="M0 34h30v30H0z"/>
             </SvgIcon>
         </IconButton>
     );
--- src/styles/AppBarStyles.js
+++ src/styles/AppBarStyles.js
@@ -6,6 +6,7 @@ import {theme, defaultStyles} from './DefaultTheme'
  */
 const fontSize = theme.fontSize;
 const height = theme.height;
+const color = theme.color;
 
 const styles = StyleSheet.create({
     barPanelStyles: {
@@ -42,6 +43,13 @@ const styles = StyleSheet.create({
     adminAvatarStyle: {
         cursor: 'pointer',
         margin: '0 20px 0 -80px'
+    },
+    msLogoSvgSt0Style: { fill: color.msRed },
+    msLogoSvgSt1Style: { fill: color.msGreen },
+    msLogoSvgSt2Style: { fill: color.msYellow },
+    msLogoSvgSt3Style: { fill: color.msBlue },
+    msLogoStyle: {
+        margin: '0 10px'
     }
 });
 export const appBarClasses = {
@@ -51,6 +59,13 @@ export const appBarClasses = {
     logoPanel: css(styles.logoPanelStyle, defaultStyles.flexRowStart),
     logoTitle: css(styles.logoTitleStyle),
     barHr: css(styles.barHrStyle),
+
     adminBarStyle: css(defaultStyles.stickyPos, defaultStyles.flexRowSpaceBetween, styles.barBaseContainerStyle, styles.adminBarStyle),
-    adminAvatar: css(styles.adminAvatarStyle)
+    adminAvatar: css(styles.adminAvatarStyle),
+
+    msLogo: css(styles.msLogoStyle),
+    msLogoSvgSt0: css(styles.msLogoSvgSt0Style),
+    msLogoSvgSt1: css(styles.msLogoSvgSt1Style),
+    msLogoSvgSt2: css(styles.msLogoSvgSt2Style),
+    msLogoSvgSt3: css(styles.msLogoSvgSt3Style)
 };
